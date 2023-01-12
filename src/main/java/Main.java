4ad81ba8c1316a17ad1bf335b6a4cdbca3f04c9d
SPARTAN-042 <sentinal042@gmail.com>
2023-01-12 09:36:25 +0000

    Embedded error handling to Main.coffeeSelection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final Coffee[] coffeesInMenu = new Coffee[] { new Espresso('S'), new Macchiato('S'), new Latte('S'), new Cappuccino('S'), new Mocha('S') };
+    private static final Coffee[] coffeesInMenu = new Coffee[]{new Espresso('S'), new Macchiato('S'), new Latte('S'), new Cappuccino('S'), new Mocha('S')};
     private static final Scanner userInput = new Scanner(System.in);
 
     public static void printMenu() {
@@ -21,70 +21,94 @@ public class Main {
 
     // userSelection method
     public static Coffee coffeeSelection() {
-        System.out.println("Please enter a number 1 - 5");
-        String coffeeChoice = userInput.nextLine();
+        do {
+            String coffeeChoice = null;
+            String coffeeSize = null;
+            try {
+                System.out.println("Please enter a number 1 - 5");
+                coffeeChoice = userInput.nextLine();
 
-        System.out.println("Please enter the size: S, M or L");
-        String coffeeSize = userInput.nextLine();
-        
-        char choice = coffeeSize.charAt(0);
-        switch (coffeeChoice) {
-            case "1":
-                return new Espresso(choice);
-            case "2":
-                return new Macchiato(choice);
-            case "3":
-                return new Latte(choice);
-            case "4":
-                return new Cappuccino(choice);
-            case "5":
-                return new Mocha(choice);
-            default:
-                System.out.println("Choice was not recognised");
-        }
+                System.out.println("Please enter the size: S, M or L");
+                coffeeSize = userInput.nextLine();
+
+                char choice = coffeeSize.charAt(0);
+                switch (coffeeChoice) {
+                    case "1":
+                        return new Espresso(choice);
+                    case "2":
+                        return new Macchiato(choice);
+                    case "3":
+                        return new Latte(choice);
+                    case "4":
+                        return new Cappuccino(choice);
+                    case "5":
+                        return new Mocha(choice);
+                    default:
+                        System.out.println("Choice was not recognised");
+                }
+            } catch (Exception e) {
+                System.out.println("Error, try again");
+            }
+
+            if (coffeeChoice.isEmpty() || coffeeSize.isEmpty()) {
+                System.out.println("One or more inputs invalid, try again.");
+            } else {
+                break;
+            }
+
+        } while (true);
         return null;
     }
 
-    public static void drinkCoffee(User user){
+    // TODO
+    //  Integrate Error Checking
+    public static void drinkCoffee(User user) {
         if (user.hasCoffee()) {
             int i = 0;
-            if (user.getCoffees().size() > 1) {
-                System.out.println("Which coffee would you like to drink:");
-                for (i = 0; i < user.getCoffees().size(); i++) {
-                    System.out.printf("%d. %s%n", i + 1, user.getCoffees().get(i).getClass().getName());
-                }
-                int choice = Integer.parseInt(userInput.nextLine());
-                System.out.printf("%s drinks their %s%n", user.getName(), user.getCoffees().get(choice - 1).getClass().getName());
-                user.getCoffees().remove(i);
-                if (user.getCoffees().size() < 2) {
+            try {
+                if (user.getCoffees().size() > 1) {
+                    System.out.println("Which coffee would you like to drink:");
+                    for (i = 0; i < user.getCoffees().size(); i++) {
+                        System.out.printf("%d. %s%n", i + 1, user.getCoffees().get(i).getClass().getName());
+                    }
+                    int choice = Integer.parseInt(userInput.nextLine());
+                    System.out.printf("%s drinks their %s%n", user.getName(), user.getCoffees().get(choice - 1).getClass().getName());
+                    user.getCoffees().remove(i);
+                    if (user.getCoffees().size() < 2) {
+                        user.setHasCoffee(false);
+                    }
+                } else {
+                    System.out.printf("%s drinks their %s%n", user.getName(), user.getCoffees().get(i).getClass().getName());
+                    user.getCoffees().remove(i);
                     user.setHasCoffee(false);
                 }
-            } else {
-                System.out.printf("%s drinks their %s%n", user.getName(), user.getCoffees().get(i).getClass().getName());
-                user.getCoffees().remove(i);
-                user.setHasCoffee(false);
+            } catch (Exception e) {
+                System.out.println("Something went wrong, try again.");
             }
+
+                
         } else {
             System.out.printf("Sorry %s, you don't have any coffee...%n", user.getName());
         }
-    }
+
+}
 
     // generateReceipt method
-    public static String generateReceipt(User user, Coffee coffee){
+    public static String generateReceipt(User user, Coffee coffee) {
         if (!affordable(user, coffee)) {
             return "You can't afford this coffee.";
         } else {
             String receiptString = String.format("-------------The Coffee Receipt for %s-------------\n", user.getName());
             receiptString += "---------------------------------------------------\n";
             receiptString += String.format("Coffee: %s(%s)", coffee.getName(), coffee.getSize());
-            receiptString += String.format("\nThe cost is: £%s", (Math.round(coffee.calculateCost(coffee.getSize()) * 100.0) / 100.0 ));
+            receiptString += String.format("\nThe cost is: £%s", (Math.round(coffee.calculateCost(coffee.getSize()) * 100.0) / 100.0));
             user.getCoffees().add(coffee);
             user.setHasCoffee(true);
             return receiptString;
         }
     }
 
-    public static boolean affordable(User user, Coffee coffee){
+    public static boolean affordable(User user, Coffee coffee) {
         double balance = user.getBalance();
         balance = (double) Math.round(balance * 100.0) / 100.0;
         if (balance < coffee.calculateCost(coffee.getSize())) {
@@ -96,7 +120,7 @@ public class Main {
         }
     }
 
-    public static void menuSelection(User user){
+    public static void menuSelection(User user) {
         System.out.printf("-------------What would you like to do %s?-------------\n%n", user.getName());
         System.out.println("-------------------------------------------------------\n");
         System.out.println("""
@@ -125,7 +149,7 @@ public class Main {
     }
 
     public static void checkBalance(User user) {
-        Double balance = user.getBalance();
+        double balance = user.getBalance();
         System.out.printf("Your current balance: £%s%n", (double) Math.round(balance * 100.0) / 100.0);
     }
 
